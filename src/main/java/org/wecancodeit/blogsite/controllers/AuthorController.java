3a3ccc8c5a18126bbe6b644ef4d8bde8b2c9fa62
Madison Lawton <madison.e.lawton@gmail.com>
2019-03-09 18:24:05 -0500

    home page styling

--- src/main/java/org/wecancodeit/blogsite/controllers/AuthorController.java
+++ src/main/java/org/wecancodeit/blogsite/controllers/AuthorController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.wecancodeit.blogsite.models.Author;
-import org.wecancodeit.blogsite.models.Category;
 import org.wecancodeit.blogsite.repositories.AuthorRepository;
 
 @Controller
